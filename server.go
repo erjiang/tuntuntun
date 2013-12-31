0b45de63c9b55216e56d8c42a9a5d5d38a95c54a
Eric Jiang <eric@doublemap.com>
2013-12-31 13:33:18 -0500

    Server receives packets on port 71

--- server.go
+++ server.go
@@ -2,6 +2,7 @@ package main
 
 import (
 	"log"
+	"net"
 )
 
 var read_buf []byte = make([]byte, BUF_SIZE)
@@ -10,5 +11,16 @@ func server() {
 	conn, err := net.ListenUDP("udp", &net.UDPAddr{IP: net.IPv4zero, Port: TUNTUNTUN_SERVER_PORT})
 	if err != nil {
 		log.Fatal(err)
+	}
+
+	for {
+		count, remote_addr, err := conn.ReadFromUDP(read_buf)
+		if err != nil {
+			log.Fatal(err)
+		}
+
+		log.Printf("Got %d bytes from %s addressed to %s",
+			count, remote_addr,
+			get_ip_dest(read_buf[ENVELOPE_LENGTH:]))
 	}
 }
