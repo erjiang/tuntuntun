0bff74ce43af3bd177bb55cd0041ed05ff71fa8a
Eric Jiang <eric@doublemap.com>
2014-01-01 16:23:17 -0500

    More changes to nat.sh

--- nat.sh
+++ nat.sh
@@ -4,12 +4,19 @@
 # turns on NAT, assuming wan_if is your Internet connection, and lan_if is
 # your internal interface.
 # based on http://www.revsys.com/writings/quicktips/nat.html
+# with more help from http://www.netfilter.org/documentation/HOWTO/NAT-HOWTO-1.html
 
 WAN="$1"
 LAN="$2"
 
+# change outgoing packets' src addr to our external IP (whatever it is)
 iptables -t nat -A POSTROUTING -o "$WAN" -j MASQUERADE
-iptables -A FORWARD -i "$WAN" -o "$LAN" -m state --state RELATED,ESTABLISHED -j ACCEPT
-iptables -A FORWARD -i "$LAN" -o "$WAN" -j ACCEPT
+
+echo 0 > /proc/sys/net/ipv4/conf/"$WAN"/rp_filter
+
+
+iptables -A FORWARD -s 192.168.8.0/24 -o "$WAN" -j ACCEPT
+# iptables -A FORWARD -i "$WAN" -o "$LAN" -m state --state RELATED,ESTABLISHED -j ACCEPT
+# iptables -A FORWARD -i "$LAN" -o "$WAN" -j ACCEPT
 
 echo 1 > /proc/sys/net/ipv4/ip_forward
