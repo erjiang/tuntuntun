39f9d8ee8e2bca7f712a961759761af1adb194e0
Eric Jiang <erjiang@indiana.edu>
2014-01-09 18:56:25 -0500

    Get DEBUG_LEVEL from environment

--- tuntuntun.go
+++ tuntuntun.go
@@ -5,6 +5,7 @@ import (
 	"log"
 	"net"
 	"os"
+	"strconv"
 )
 
 const TUNTUNTUN_CLIENT_PORT int = 70
@@ -14,10 +15,19 @@ const TUNTUNTUN_SERVER_PORT int = 71
 
 const BUF_SIZE uint = 2048
 
+var DEBUG_LEVEL int = 0
+
 func main() {
 	if len(os.Args) < 2 {
 		fmt.Printf("server or client?")
 		return
+	}
+
+	// check if DEBUG is a valid number, and if so
+	// put it in the global DEBUG_LEVEL var
+	env_debug, err := strconv.ParseInt(os.Getenv("DEBUG_LEVEL"), 10, 32)
+	if err == nil && env_debug > 0 {
+		DEBUG_LEVEL = int(env_debug)
 	}
 
 	if os.Args[1] == "client" {
