3117c483ebcfa80207c517834b708f8de6a54bd9
Eric Jiang <erjiang@indiana.edu>
2014-01-27 20:43:31 -0500

    Switch between two buffers when listening

--- listen.go
+++ listen.go
@@ -29,22 +29,35 @@ func listenUDP(conn UDPReadWrite, c chan UDPRecv) error {
 
 	debugf(1, "%sListening to %p...%s", mycolor, conn, ansi_reset)
 
+	// switch between two buffers so we don't clobber something that the end
+	// end of the pipe is busy processing
 	read_buf := make([]byte, BUF_SIZE)
+	read_buf2 := make([]byte, BUF_SIZE)
+	curr_buf := &read_buf
+	other_buf := &read_buf2
 	for {
-		count, remote_addr, err := conn.ReadFromUDP(read_buf)
+		count, remote_addr, err := conn.ReadFromUDP(*curr_buf)
 		if err != nil {
 			log.Print(err)
 			return err
 		}
 		debugf(3, "%sGot %d bytes from %s%s", mycolor, count, remote_addr, ansi_reset)
+		// some debugging by printing out packet numbers
+		if count > 5 {
+			e := parse_envelope(read_buf)
+			debugf(4, "rfUDP: %d", e.sequence)
+		}
 		c <- UDPRecv{
-			Data:       read_buf[:count],
+			Data:       (*curr_buf)[:count],
 			RemoteAddr: remote_addr,
 		}
 		if DEBUG_LEVEL >= 1 {
 			fmt.Print(mycolor, "R", ansi_reset)
 		}
-
+		// swap the buffers
+		temp_swp := curr_buf
+		curr_buf = other_buf
+		other_buf = temp_swp
 	}
 }
 
